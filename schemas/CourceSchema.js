b634ec2c29122565366c442b27179b36839c2b15
nirmalkumar52106 <104349784+nirmalkumar52106@users.noreply.github.com>
2025-01-31 17:06:03 +0530

    first commit

--- schemas/CourceSchema.js
+++ schemas/CourceSchema.js
@@ -5,6 +5,6 @@ const CourceSchema = new mongoose.Schema({
     courcename : String,
     courcedescription : String,
   }); 
-  const Cource = mongoose.model('newemployes', CourceSchema);
+  const Cource = mongoose.model('newcources', CourceSchema);
 
   module.exports=Cource
