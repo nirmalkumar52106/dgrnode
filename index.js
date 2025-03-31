69a4c4a88cb92e63d63c10ffe9b9f8e140b4d7f4
nirmalkumar52106 <104349784+nirmalkumar52106@users.noreply.github.com>
2025-03-31 20:05:53 +0530

    first commit

--- index.js
+++ index.js
@@ -149,7 +149,6 @@ app.get("/allblog/:id", async(req,res)=>{
     const slug = req.params.id;
     const result = await Blog.findOne({slugurl:slug});
     res.json(result);
-    res.send(result)
 });
 
 //get by id
