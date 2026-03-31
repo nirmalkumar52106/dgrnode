a007c14f556450501363b8400b799123bae704c2
nirmalkumar52106 <104349784+nirmalkumar52106@users.noreply.github.com>
2026-03-31 17:15:30 +0530

    first

--- schemas/mongodb.js
+++ schemas/mongodb.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose")
 const dburl = "mongodb+srv://kumarnirmal52106:C60nVUk493GG9sw1@cluster0.utzu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(dburl , {
-    useNewUrlParser: true,
-      useUnifiedTopology: true, 
       family: 4 
 }).then(()=>{
     console.log("success")
