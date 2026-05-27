1f4e86f59cce457d016baa336622f3f83865b12a
Nirmal Kumar <104349784+nirmalkumar52106@users.noreply.github.com>
2026-05-27 11:42:23 +0530

    Fix syntax error in Course.find query

--- index.js
+++ index.js
@@ -1060,7 +1060,7 @@ app.post("/addcourse" , verifyAdminOrStaff, async(req,res)=>{
 })
 
 app.get("/allcourse",async(req,res)=>{
-    const courses = await Course.find({)
+    const courses = await Course.find({})
     res.send(courses)
 })
 
